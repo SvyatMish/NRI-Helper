d74bf497ae7b4fbbfe442fcf793aab6d07d5b72d
Svyat <svyatoslav.mish@yandex.ru>
2024-10-21 13:44:53 +0300

    feat: button styles

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
   export let onClick: VoidFunction = () => {
   };
+  export let className: string | undefined = undefined;
 </script>
 
-<button on:click={onClick}>
+<button
+  class={`bg-gray-200 shadow text-gray-800 rounded cursor-pointer box-border p-2 flex content-center justify-center hover:bg-gray-400 active:bg-gray-500 font-bold duration-100 ${className ? className : ''}`}
+  on:click={onClick}>
   <slot />
 </button>
