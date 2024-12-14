a1175b117206933b7ae651815dea29e7eb7370a9
Мишин Святослав <mishinss@m2.ru>
2024-12-14 19:32:39 +0700

    feat: more fixes

--- src/App.tsx
+++ src/App.tsx
@@ -11,22 +11,19 @@ import { FormulaList } from "./components/FormulaList.tsx";
 
 const darkTheme = createTheme({
   palette: {
-    mode: "dark"
-  }
+    mode: "dark",
+  },
 });
-
 
 function App() {
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
       <main className="box-border grid grid-cols-[1fr_1fr] gap-8">
-        <RollForm />
+        <RollForm initialValues={{ difficulty: 6 }} />
         <FormulaList />
       </main>
     </ThemeProvider>
-
-
   );
 }
 
--- src/components/RollForm.tsx
+++ src/components/RollForm.tsx
@@ -2,22 +2,29 @@ import { useForm } from "react-hook-form";
 import { Button, Typography } from "@mui/material";
 
 import { RHInput } from "./inputs.tsx";
-import { useState } from "react";
+import React, { useState } from "react";
 import { RollResultType } from "../types";
 import { RollResult } from "./RollResult.tsx";
 import { roll } from "../utils";
 
 interface Values {
-  rollAmount: number;
+  rollAmount?: number;
   difficulty?: number;
 }
 
-export const RollForm = () => {
+export const RollForm: React.FC<{ initialValues?: Values }> = ({
+  initialValues,
+}) => {
   const [results, setResults] = useState<RollResultType[]>([]);
-  const { control, handleSubmit } = useForm<Values>();
+  const { control, handleSubmit } = useForm<Values>({
+    defaultValues: initialValues,
+  });
 
   const onSubmit = (data: Values) => {
-    setResults((prev) => [roll(data.rollAmount, data.difficulty), ...prev]);
+    if (!data.rollAmount || !data.difficulty) {
+      return;
+    }
+    setResults((prev) => [roll(data.rollAmount!, data.difficulty), ...prev]);
   };
 
   const onClear = () => {
@@ -26,7 +33,9 @@ export const RollForm = () => {
 
   return (
     <div className="min-w-full p-6 box-border h-screen overflow-hidden flex flex-col">
-      <Typography className="mb-10" variant="h3">Роличная</Typography>
+      <Typography className="mb-10" variant="h3">
+        Роличная
+      </Typography>
       <form className="flex items-end w-fit" onSubmit={handleSubmit(onSubmit)}>
         <RHInput name="rollAmount" control={control} label="Сколько роллить" />
         <RHInput name="difficulty" control={control} label="Сложность" />
@@ -34,15 +43,10 @@ export const RollForm = () => {
         <Button onClick={onClear}>Очистить результат</Button>
       </form>
       <div className="flex-1 overflow-auto max-w-[640px]">
-        {results.map((result, index) => <RollResult result={result} key={index} />)}
+        {results.map((result, index) => (
+          <RollResult result={result} key={index} />
+        ))}
       </div>
     </div>
-
   );
 };
-
-
-
-
-
-
