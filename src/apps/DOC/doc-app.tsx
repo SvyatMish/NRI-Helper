71cffc8d0b119784f56884562aa673ac9da0b27f
Мишин Святослав <mishinss@m2.ru>
2025-05-31 18:58:06 +0300

    feat: add fantasy font

--- src/apps/DOC/doc-app.tsx
+++ src/apps/DOC/doc-app.tsx
@@ -2,8 +2,14 @@ import { ParchmentContainer } from "../../components/ParchmentContainer";
 
 export const DocApp = () => {
   return (
-    <ParchmentContainer className="flex w-full min-h-screen">
-      123123
+    <ParchmentContainer className="flex w-full min-h-screen kereru">
+      123123 Здарова Бродяга
+      <b>Здарова Бродяга</b>
+      <i>Здарова Бродяга</i>
+      <h1>Здарова Бродяга</h1>
+      <b>
+        <i>Plfhjdf</i>
+      </b>
     </ParchmentContainer>
   );
 };
